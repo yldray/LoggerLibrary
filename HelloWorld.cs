513dcc512e46d640d5176d0b8bd6f48c8dbb70c8
= <yildirayt@idealdata.com.tr>
2022-09-12 16:14:54 +0300

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using LoggerLibrary.Logging.Log4Net.Loggers;
+using LoggerLibrary.LoggingAttribute;
+
+namespace LoggerLibrary
+{
+    
+    [LogAspect(typeof(FileLogger))]
+    public class HelloWorld
+    {
+      
+    }
+}
---
+++ Logging/Log4Net/Loggers/DatabaseLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggerLibrary.Logging.Log4Net.Loggers
+{
+    public class DatabaseLogger:LoggerServiceBase
+    {
+        public DatabaseLogger() : base("DatabaseLogger")
+        {
+        }
+    }
+}
